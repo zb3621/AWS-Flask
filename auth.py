d8583386994dda867119bcf4d414a8303a3372e2
Tayyab <mtayyab1143@hotmail.com>
2021-03-25 17:13:38 -0400

    initial commit

---
+++ auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+# @auth.route('/login')
+# def login():
+#     return render_template("login.html")
+
+# @auth.route('/logout')
+# def logout():
+#     return "<p>Logout</p>"
+
+# @auth.route('/Sign-up')
+# def Sign_up():
+#     return render_template("sign_up.html")
